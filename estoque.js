ea0dc21fd4290cef7410d4ffd5b78639c1779d40
wilian santos <14809032647@estudante.ufjf.br>
2023-12-06 20:58:58 -0300

    atividade 12 iniciada

---
+++ estoque.js
@@ -0,0 +1,21 @@
+//estoque.js
+
+let estoque ={
+
+ 'joao ': [
+    {'tipo': "maca",'qtd': 1},
+    {'tipo': "pera", 'qtd':1 }
+ ],
+ maria: [
+    {'tipo': "maca",'qtd': 2},
+    {'tipo': "banana", 'qtd':4}
+ ]
+};
+
+
+export function getEstoque(){
+    return  structuredClone(estoque);
+}
+
+
+estoque['joao'] [0].tipo
