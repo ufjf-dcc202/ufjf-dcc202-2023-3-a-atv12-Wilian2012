6e0eb98ede810fa9990ca43c891ef6fff3f843e0
wilian santos <14809032647@estudante.ufjf.br>
2023-12-13 20:59:09 -0300

    continuando a atividade 12

--- estoque.js
+++ estoque.js
@@ -2,11 +2,11 @@
 
 let estoque ={
 
- 'joao ': [
+ 'joao': [
     {'tipo': "maca",'qtd': 1},
     {'tipo': "pera", 'qtd':1 }
  ],
- maria: [
+ 'maria': [
     {'tipo': "maca",'qtd': 2},
     {'tipo': "banana", 'qtd':4}
  ]
@@ -18,4 +18,55 @@ export function getEstoque(){
 }
 
 
-estoque['joao'] [0].tipo
+//estoque['joao'] [0].tipo
+
+export function transacao(origem, destino,quantidade,fruta){
+
+   if ( origem === destino){
+      return;
+   }
+   if(origem === 'pomar'){
+      dePomarParaPessoa(destino, quantidade, fruta);
+   }
+   if(destino === "pomar"){
+      dePessoaParaPomar(origem, quantidade, fruta);
+   }
+
+function dePomarParaPessoa(destino, quantidade, fruta){
+
+   const pessoa =estoque[destino];
+      let monte ;
+      for(let i=0; i<pessoa.length;i++){
+        
+         if(pessoa[i].tipo === fruta){
+            monte = pessoa[i];
+            break;
+         }
+      }
+         if(!monte){
+            monte={tipo: fruta, qtd:0};
+            pessoa.push(monte);
+         }
+         monte.qtd += quantidade;
+                
+      }
+
+}
+   
+function dePessoaParaPomar(origem, quantidade, fruta){
+
+   const pessoa = estoque[origem];
+         let monte;
+         for(let i=0; i<pessoa.length;i++){
+        
+            if(pessoa[i].tipo === fruta){
+               monte = pessoa[i];
+               break;
+            }
+         }
+            if(!monte){
+               return;
+            }
+            monte.qtd += Math.min(quantidade, monte.qtd);
+                
+         }
